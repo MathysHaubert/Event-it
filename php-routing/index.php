0ea2563cf4a664a2e4dddf77612589e2e8658643
MathysHaubert <mathys.haubert80@gmail.com>
2024-02-26 11:59:38 +0100

    Update README.md and router.php, and add setup instructions

--- php-routing/index.php
+++ php-routing/index.php
@@ -1,11 +1,9 @@
 <?php
 
-require_once ('router.php');
+require_once 'router.php';
 
 $router = new Router();
 
-$router->loadRoutes('routes.yaml');
-
-require_once ('router.php');
+$router->loadRoutes(__DIR__ . '/routes.yaml');
 
 $url = $_SERVER['REQUEST_URI']; 
