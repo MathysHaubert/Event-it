1618da194ccfe72c4db1897c477de2e9549c5749
Mathys Haubert <mathys.haubert80@gmail.com>
2024-03-08 00:00:44 +0100

    add setup sh

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,41 +2,104 @@
 
 namespace App\Entity;
 
+use App\Entity\Client\Client;
+use App\Trait\Identifier;
 use DateTime;
 
 class User
 {
-    private int $id;
-    private string $lastName;
-    private string $firstName;
-    private DateTime $createdAt;
-    private DateTime $lastConnection;
-    private int $clientId;
-    private string $password;
-    private string $email;
-    private int $organizationId;
+    use Identifier;
 
     public function __construct(
-        int $id,
-        string $lastName,
-        string $firstName,
-        DateTime $createdAt,
-        DateTime $lastConnection,
-        int $clientId,
-        string $password,
-        string $email,
-        int $organizationId
+        private string $lastname,
+        private string $firstname,
+        private DateTime $createAt,
+        private DateTime $lastConnection,
+        private Client $client,
+        private string $password,
+        private string $email,
+        private Organization $organization
     ) {
-        $this->id = $id;
-        $this->lastName = $lastName;
-        $this->firstName = $firstName;
-        $this->createdAt = $createdAt;
-        $this->lastConnection = $lastConnection;
-        $this->clientId = $clientId;
-        $this->password = $password;
-        $this->email = $email;
-        $this->organizationId = $organizationId;
     }
 
+    public function getLastname(): string
+    {
+        return $this->lastname;
+    }
+
+    public function setLastname(string $lastname): void
+    {
+        $this->lastname = $lastname;
+    }
+
+    public function getFirstname(): string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(string $firstname): void
+    {
+        $this->firstname = $firstname;
+    }
+
+    public function getCreateAt(): DateTime
+    {
+        return $this->createAt;
+    }
+
+    public function setCreateAt(DateTime $createAt): void
+    {
+        $this->createAt = $createAt;
+    }
+
+    public function getLastConnection(): DateTime
+    {
+        return $this->lastConnection;
+    }
+
+    public function setLastConnection(DateTime $lastConnection): void
+    {
+        $this->lastConnection = $lastConnection;
+    }
+
+    public function getClient(): Client
+    {
+        return $this->client;
+    }
+
+    public function setClient(Client $client): void
+    {
+        $this->client = $client;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): void
+    {
+        $this->password = $password;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    public function getOrganization(): Organization
+    {
+        return $this->organization;
+    }
+
+    public function setOrganization(Organization $organization): void
+    {
+        $this->organization = $organization;
+    }
     
 }
