9d39210d31026e83d9485409c914dc04363b4973
hamzath-Ade <hamzathyessoufou@gmail.com>
2024-04-16 18:09:30 +0200

    comit message

---
+++ templates/public/homePage/homePage.js
@@ -0,0 +1,32 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slide");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
+
+// Attach event listeners to buttons
+document.querySelector(".prev").addEventListener("click", function() {
+  plusSlides(-1);
+});
+
+document.querySelector(".next").addEventListener("click", function() {
+  plusSlides(1);
+});
