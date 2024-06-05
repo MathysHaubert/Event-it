634e1ccc5975b041f32f0c7920d0dbf5bad6e4a9
Victor GERMAIN <victor.germain@eleve.isep.fr>
2024-06-05 08:19:00 +0200

    Started work on the user data changes but I have to go to work so we will see tonight

--- public_html/src/Controller/UserProfile/UserProfileController.php
+++ public_html/src/Controller/UserProfile/UserProfileController.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace App\Controller\UserProfile;
 
+use Api;
 use App\Controller\Controller;
 use App\Cookie\CookieHandler;
+use App\Entity\User\User;
 
 class UserProfileController extends Controller{
     public function index($data = []): void
@@ -19,6 +21,10 @@ class UserProfileController extends Controller{
         exit();
     }
 
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $this->updateUser($_POST);
+    }
+
     // define the default locale at french
     $this->webRender('public/UserProfile/' . self::INDEX, [
         'title' => 'Home Page',
@@ -27,4 +33,45 @@ class UserProfileController extends Controller{
         'user' => $currentUser,
     ]);
 }
+
+public function updateUser($data): void
+    {
+        if (!isset($data['id'])) {
+            error_log("User ID is missing.");
+            return;
+        }
+
+        $userId = $data['id'];
+        $user = (new User())->getUser(["id" => $userId]);
+
+        if (!$user) {
+            error_log("User not found for ID: $userId");
+            return;
+        }
+
+        if (isset($data['email'])) {
+            $user->setEmail($data['email']);
+        }
+
+        if (isset($data['first_name'])) {
+            $user->setFirstName($data['first_name']);
+        }
+
+        if (isset($data['last_name'])) {
+            $user->setLastName($data['last_name']);
+        }
+
+        if (isset($data['password'])) {
+            $user->setPassword($data['password']);
+        }
+
+        $response = $user->updateUser([
+            'id' => $userId,
+            'email' => $user->getEmail(),
+            'first_name' => $user->getFirstName(),
+            'last_name' => $user->getLastName(),
+            'password' => $user->getPassword(),
+        ]);
+        error_log(json_encode($response));
+    }
 }
